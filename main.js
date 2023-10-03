2cc91e5e2dc5c530a3f392ee1e9c1f529a3a6816
Zuzia <zuzia.zur@gmail.com>
2023-10-03 12:18:39 +0200

    css changes

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 
 const button = document.querySelector('.page-header__button--js');
+const clickCountElement = document.getElementById('clickCount');
+let clickCount = 0;
+
 const clickFunction = (e) => {
     const header = document.querySelector('.person__name--js');
-    header.innerHTML = 'klik, klik!'
-    
+    header.innerHTML = 'klik, klik!';
+    clickCount++;
+
+    clickCountElement.textContent = clickCount;
 }
 
-button.addEventListener('click', clickFunction)
+button.addEventListener('click', clickFunction);
